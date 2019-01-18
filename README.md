2de1ba29bff52870445fdc2f677e17b56e1b360a
pdaniels4143 <pdaniels4143@student.rcschools.net>
2019-01-18 14:04:23 -0600

    Began extra assignment.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Extra-Dice-Assignment
 What to work on when finished with other stuff in coding. Due: Feb. 8th.
+
+Write a program that performs a simulation to estimate the probability of rolling 5 of a kind in a single roll of five six-sided dice. 
+
+ 
+
+Include graphics to display the results of each roll.  
--- dice.py
+++ dice.py
@@ -1,1 +1,31 @@
+#Parker Daniels
+#Estimates probability of rolling 5 of a kind on 6-sided dice while also displaying graphics for the dice.
 
+import graphics as g
+import tkinter
+import random
+
+def main():
+    intro()
+    dice=setup()
+
+
+
+    tkinter.mainloop()
+def intro():
+    print("Rolls five dice simultaneously a given number of times and then outputs the number of times a 5 of a kind"
+          " set was rolled and the percentage calculated.")
+
+#plan to use dictionaries to store dice graphics. try 800 pix.
+def setup():
+    graph=g.GraphWin("Dice Probability",800,800)
+    dice={}
+
+
+
+
+
+
+
+
+main()
